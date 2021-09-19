65b97abf77a5afd5d7a2578a1c7a38fe03c3b109
victorlee <myleevictor@gmail.com>
2021-09-19 13:20:46 +0800

    Use correct Lottery artifact in migrations

--- migrations/1_deploy_lottery.js
+++ migrations/1_deploy_lottery.js
@@ -1,5 +1,5 @@
-const Lottery = artifacts.require('./Lottery.sol');
+const Lottery = artifacts.require('Lottery');
 
-module.exports = function (deployer) {
-  deployer.deploy(Lottery);
+module.exports = async function (deployer) {
+  await deployer.deploy(Lottery)
 };
